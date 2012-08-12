4da04b9705a1ad0f732c8e0412157fabd55e5625
Carter Schoenfeld <carter.schoenfeld@gmail.com>
2012-08-12 01:01:25 -0400

    Added example for plugin as well as modified the plugin to fix an issue with the bottom padding of the outer div

--- showMore/jquery.showMore.js
+++ showMore/jquery.showMore.js
@@ -9,7 +9,7 @@
     
     return this.each(function() {
       var elements, lineHeight, childHeight, 
-      elementsHeight, parentElement,
+      elementsHeight, parentElement, showMoreID,
       parentHeight, newHeightMultiplier, hideOverflow;
       
       elements        = $(this).children();
@@ -17,7 +17,7 @@
       parentHeight    = $(this).parent().height();
       elementsHeight  = $(this).height();
       
-      hideOverflow = function($idx){
+      var hideOverflow = function($idx){
         for($idx; $idx < elements.length; $idx++){
           elements.eq($idx).hide();
         }
@@ -27,18 +27,38 @@
       
       if(elementsHeight > parentHeight){
         for($i = 0; $i < elements.length; $i++){
-          childHeight = elements.eq($i).height();
+          childHeight = elements.eq($i).outerHeight(true);
+          
           if(childHeight < parentHeight){
             parentHeight = parentHeight - childHeight;
           } else {
             hideOverflow($i + 1);
             lineHeight = parseInt(elements.eq($i).css('line-height'));
             newHeightMultiplier = Math.floor(parentHeight/lineHeight) - 1;
+            showMoreID = Math.floor(Math.random(1,1000) * 1000);
+            
+            
             elements.eq($i).height(newHeightMultiplier * lineHeight);
-            $(this).after('<span class="' + settings.styleClass + '"><a href="'+ settings.link +'">'+ settings.linkText +'</a></span>');
+            elements.eq($i).css({'overflow' : 'hidden'});
+            
+            $(this).after('<span id="'+ showMoreID +'" class="' + settings.styleClass + '"><a href="'+ settings.link +'">'+ settings.linkText +'</a></span>');
             break;
           }
         }
+      }
+      
+      if(settings.link === 'javascript:void(0)' || settings.link === '#'){
+         var that = this;
+         $('#' + showMoreID).click(function(){
+            var paddingBottom = parseInt($(this).parent().css('padding-bottom')); 
+            $(this).parent().height(elementsHeight + (paddingBottom * 2));
+            $(that).children().css({
+                  'overflow' : 'visible',
+                  'height' : 'auto'
+                  });
+            $(that).children().show();
+            $(this).hide();
+         });
       }
 
     });
